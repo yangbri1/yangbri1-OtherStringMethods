785533901dd73d0da87468931219d0301d062140
Brian Yang <yangbri1@msu.edu>
2024-12-04 02:34:49 +0000

    Use .substring(), .compareTo(), .split() String methods

--- src/main/java/OtherStringMethods.java
+++ src/main/java/OtherStringMethods.java
@@ -11,7 +11,7 @@ public class OtherStringMethods {
      * @return
      */
     public String partOfString(String s, int start, int end){
-        return "";
+        return s.substring(start, end);
     }
 
     /**
@@ -26,7 +26,7 @@ public class OtherStringMethods {
      * and 0 if s1 is equivalent to s2.
      */
     public int compareLexigraphically(String s1, String s2) {
-        return 0;
+        return s1.compareTo(s2);    // compare character by character
     }
 
     /**
@@ -39,6 +39,6 @@ public class OtherStringMethods {
      * @return an array of Strings that represent s1 split by splitAround.
      */
     public String[] splitStringIntoMultipleStrings(String s1, String splitAround) {
-        return null;
+        return s1.split(splitAround);
     }
 }
